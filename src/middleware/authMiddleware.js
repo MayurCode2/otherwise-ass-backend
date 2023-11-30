fbe3f91ee434026758f012d8d71b8fe3893a9f55
mayur patil <mayurpatil@mayurs-MacBook-Pro.local>
2023-11-30 09:37:42 +0530

    middleware changes

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const verifyToken = (req, res, next) => {
   if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
   try {
-    const decoded = jwt.verify(token, config.secretKey);
+    const decoded = jwt.verify(token,"your-secret-key");
     req.user = decoded.user;
     next();
   } catch (error) {
